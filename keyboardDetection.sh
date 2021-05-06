2ce0ce2c76bf338d0c95b3172b7f93098d8a7173
0ur0b0r0s <pedro_souza@riseup.net>
2021-05-06 01:15:12 -0300

    bash to dash

--- keyboardDetection.sh
+++ keyboardDetection.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 CHECK_KEYBOARD=$(lsusb | grep "HyperX")
 
-if [[ $CHECK_KEYBOARD =~ "HyperX" ]];then
+if [ $CHECK_KEYBOARD =~ "Bus 002 Device 002: ID 0951:16c6 Kingston Technology HyperX Mars Gaming KeyBoard" ];then
     setxkbmap us
     xmodmap ~/.Xmodmap
 else
