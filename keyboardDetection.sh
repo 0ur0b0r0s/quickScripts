9f5ef39e82df7de5d0ecbb0e7483bfcd79d03a27
0ur0b0r0s <pedro_souza@riseup.net>
2021-05-06 01:48:36 -0300

    minor syntax change

--- keyboardDetection.sh
+++ keyboardDetection.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 CHECK_KEYBOARD=$(lsusb | grep "HyperX")
+DEVICE_NAME="Bus 002 Device 002: ID 0951:16c6 Kingston Technology HyperX Mars Gaming KeyBoard"
 
-if [ $CHECK_KEYBOARD =~ "Bus 002 Device 002: ID 0951:16c6 Kingston Technology HyperX Mars Gaming KeyBoard" ];then
+if [ "$CHECK_KEYBOARD" = "$DEVICE_NAME" ];then
     setxkbmap us
-    xmodmap ~/.Xmodmap
+    setxkbmap -option "ctrl:nocaps"
 else
     setxkbmap br
-    xmodmap ~/.Xmodmap
+    setxkbmap -option "ctrl:nocaps"
 fi
